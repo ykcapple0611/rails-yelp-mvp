75a5cf70dc10baa7af4b7ab31333601bbcf9b22b
christina choi <christina.yk.choi@gmail.com>
2022-11-10 17:42:31 +0800

    final version with rpsec passed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, :rating, presence: true
-  # validates :rating, inclusion: { in: rand(0..5) }
-  validates :rating, :inclusion => 1..5
+  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
